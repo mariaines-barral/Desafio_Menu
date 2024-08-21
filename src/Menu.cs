4225376e1b53f9f3e7691d731f22715cacb5995d
Ines Barral <barral.mariaines@gmail.com>
2024-08-21 11:46:01 -0300

    Added expected methods to Menu class

--- src/Menu.cs
+++ src/Menu.cs
@@ -9,5 +9,27 @@ using System.Collections;
 public class Menu
 {
     private ArrayList dishes = new ArrayList();
+
+    public void AddDish(Dish)
+    {
+        dishes.Add(Dish);
+    }
+
+    public void RemoveDish(Dish)
+    {
+        dishes.Remove(Dish)
+    }
+
+    public Dish GetDishByName(string dishName)
+    {
+        foreach (var dish in dishes)
+        {
+            if (dish.Name == dishName)
+            {
+                return dish
+            }
+            else return null
+        }
+    }
 }
 
