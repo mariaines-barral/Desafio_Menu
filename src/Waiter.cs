f2def916c63dc98304d20a981e50175f7fc08e8a
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-08-21 17:40:18 -0300

    Waiter Finalizado

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,5 +7,33 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Waiter
 {
+    public string Name;
+
+    public string name
+    {
+        get { return name; } set { name = value; }
+    }
+    
     private ArrayList assignedTables = new ArrayList();
+
+    public void AssignTable(Table table)
+    {
+        assignedTables.Add(table);
+    }
+    
+    public void TakeOrder(Table table, Dish dish)
+    {
+        if (assignedTables.Contains(table))
+        { 
+            table.AddToOrder(dish);
+        }
+    }
+    
+    public Waiter(string name)
+    {
+        {
+            this.Name = name;
+        }
+    }
+    
 }
